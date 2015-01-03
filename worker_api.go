cc1c127ca3a9093f94d0b6f1366593166f8a5d21
Kevin Manley <kevin.manley+github@gmail.com>
2015-01-02 21:00:29 -0500

    added worker_api.go

---
+++ worker_api.go
@@ -0,0 +1,40 @@
+package midtown
+
+import (
+	"fmt"
+	"github.com/golang/glog"
+	"net"
+	"net/rpc"
+)
+
+type WorkerApi struct {
+	model *Model
+}
+
+func NewWorkerApi(model *Model) *WorkerApi {
+	return &WorkerApi{model}
+}
+
+func (this *WorkerApi) GetWorkerTask(workerName string, task **WorkerTask) error {
+	t, err := this.model.GetWorkerTask(workerName)
+	*task = t
+	return err
+}
+
+// TODO: port e.g. ":9999"
+func StartWorkerAPI(model *Model, port string) {
+	rpc.Register(NewWorkerApi(model))
+	glog.Infof("serving worker API on %s", port)
+	ln, err := net.Listen("tcp", port)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(c)
+	}
+}
